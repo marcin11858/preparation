6657bd62848302895131537eb6de4a6a1b3bb517
marcin11858 <wlazlymarcin@gmail.com>
2020-01-14 21:38:23 +0100

    Second chapter, part two

--- src/second/SecondChapter.java
+++ src/second/SecondChapter.java
@@ -8,7 +8,9 @@ public class SecondChapter {
 //        question_2_1();
 //        question_2_1_1();
 //        question_2_2();
-        question_2_3();
+//        question_2_3();
+//        question_2_4();
+        question_2_5();
     }
 
 
@@ -101,5 +103,53 @@ public class SecondChapter {
         n.next = n.next.next;
         return true;
     }
+
+    private static void question_2_4() {
+            LinkedListNode node = LinkedListNode.buildList(new int[]{3, 5, 8, 5, 10, 2, 1});
+            int x = 5;
+
+            LinkedListNode head = node;
+            LinkedListNode tail = node;
+
+            while (node != null) {
+                LinkedListNode next = node.next;
+                if (node.data < x) {
+                    node.next = head;
+                    head = node;
+                } else {
+                    tail.next = node;
+                    tail = node;
+                }
+                node = next;
+            }
+            tail.next = null;
+
+            LinkedListNode.printList(head);
+    }
+
+    private static void question_2_5() {
+        LinkedListNode first = LinkedListNode.buildList(new int[]{7, 1, 6});
+        LinkedListNode second = LinkedListNode.buildList(new int[]{5, 9, 6});
+        LinkedListNode result = null;
+        LinkedListNode head = null;
+
+        int nextValue = 0;
+        while (first != null || second != null || nextValue > 0) {
+            int sum = (first != null ? first.data : 0)  + (second != null ? second.data : 0) + nextValue;
+            if (result == null) {
+                result = new LinkedListNode(sum % 10);
+                head = result;
+            } else {
+                result.next = new LinkedListNode(sum % 10);
+                result = result.next;
+            }
+            nextValue = (int) sum / 10;
+            first = (first != null ? first.next : null);
+            second = (second != null ? second.next : null);
+        }
+
+        LinkedListNode.printList(head);
+    }
+
 
 }
